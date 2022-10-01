55659ef8d8e31cd9ccda8528c8085a6b775a3ee8
miathemage <mine.aranda545@gmail.com>
2022-10-01 13:02:34 -0500

    Changes to structure and code.
    Changes to CSS.
    Added JavaScript functions to change the temperature from Celsius to Fahrenheit and viceversa.

--- src/script.js
+++ src/script.js
@@ -16,38 +16,16 @@ let currentMinutes = ("0" + date.getMinutes()).slice(-2);
 let h6 = document.querySelector("h6");
 h6.innerHTML = `${currentDay} ${currentHours}:${currentMinutes}`;
 
-function defaultTemp(response) {
-  console.log(response.data);
-  let defaultTemperature = document.querySelector("#current-degree");
-  let defaultCity = document.querySelector("#current-city");
-  defaultTemperature.innerHTML = Math.round(response.data.main.temp);
-  defaultCity.innerHTML = city;
-}
-
-let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
-let city = "Mexico City";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(defaultTemp);
-
-function searchCity(city) {
-  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayCurrentWeather);
-}
-
-function search(event) {
-  event.preventDefault();
-  let city = document.querySelector("#search-city").value;
-  searchCity(city);
-}
-
 function displayCurrentWeather(response) {
   console.log(response.data);
-  document.querySelector("#current-city").innerHTML = response.data.name;
   let degreeNumber = Math.round(response.data.main.temp);
   let humidNumber = Math.round(response.data.main.humidity);
   let windNumber = Math.round(response.data.wind.speed);
-  document.querySelector("#current-degree").innerHTML = `${degreeNumber}`;
+
+  celsiusTemp = Math.round(response.data.main.temp);
+
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-degree").innerHTML = `${celsiusTemp}`;
   document.querySelector("#current-humidity").innerHTML = `${humidNumber}%`;
   document.querySelector("#current-wind").innerHTML = `${windNumber} km/h`;
   let iconWeather = document.querySelector("#icon");
@@ -58,8 +36,48 @@ function displayCurrentWeather(response) {
   iconWeather.setAttribute("alt", response.data.weather[0].description);
 }
 
+function searchCity(city) {
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city");
+  searchCity(city.value);
+}
+
+function showFahrenheitTemp(event) {
+  event.preventDefault();
+  let fahrenheitMath = (celsiusTemp * 9) / 5 + 32;
+  // remove class from celsius link
+  celsiusLink.classList.remove("unit");
+  fahrenheitLink.classList.add("unit");
+  let degreeNumber = document.querySelector("#current-degree");
+  degreeNumber.innerHTML = Math.round(fahrenheitMath);
+}
+
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("unit");
+  fahrenheitLink.classList.remove("unit");
+  let degreeNumber = document.querySelector("#current-degree");
+  degreeNumber.innerHTML = celsiusTemp;
+}
+
+let celsiusTemp = null;
+
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#farenheit");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+searchCity("Mexico City");
 
 function searchCurrentLocation(position) {
   let lat = position.coords.latitude;
@@ -76,21 +94,3 @@ function currentLocation(event) {
 
 let buttonCurrentLocation = document.querySelector("#current-location");
 buttonCurrentLocation.addEventListener("click", currentLocation);
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let currentCelsius = document.querySelector("#current-degree");
-  currentCelsius.innerHTML = 21;
-}
-
-function convertToFarenheit(event) {
-  event.preventDefault();
-  let currentFarenheit = document.querySelector("#current-degree");
-  currentFarenheit.innerHTML = Math.round(21 * 1.7 + 32);
-}
-
-let changeCelsius = document.querySelector("#celsius");
-changeCelsius.addEventListener("click", convertToCelsius);
-
-let changeFarenheit = document.querySelector("#farenheit");
-changeFarenheit.addEventListener("click", convertToFarenheit);
